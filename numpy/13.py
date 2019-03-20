0cf471bc967bcb5990535ce859ce0a18d1d787e5
Student-6 <sarentio.mika@gmail.com>
2019-03-20 18:58:49 +0900

    New tasks

---
+++ numpy/13.py
@@ -0,0 +1,4 @@
+import numpy as np
+X = np.random.random((10,10))
+Xmin, Xmax = X.min(), X.max()
+print(Xmin, Xmax)
---
+++ numpy/14.py
@@ -0,0 +1,4 @@
+import numpy as np
+X = np.random.random(30)
+kek = X.mean()
+print(kek)
